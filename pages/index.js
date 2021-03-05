bd514a917c759a5b40e73c650a1925028a5ea57d
andrelobo <loboandre@hotmail.com>
2021-03-04 20:10:33 -0400

    card changes

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           <a href="https://www.instagram.com/pico_de_jaca/" className="card">
             <h3>INSTAGRAM</h3>
           </a>
-          <a href="https://api.whatsapp.com/send?phone=5592981493108&text=Texto%20aqui" className="card">
+          <a href="https://api.whatsapp.com/send?phone=559294435181&text=Texto%20aqui" className="card">
           <h3>WHATSAPP</h3>
           </a>
           <a href="https://ruaearte.com.br/pico-de-jaca/" className="card">
